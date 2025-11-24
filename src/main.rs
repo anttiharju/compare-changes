bb3106760947a0323e5c4f2552a9ff9a6fb4c21c
Antti Harju <antti@harju.io>
2025-11-24 22:52:48 +0100

    Remove config

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::path::PathBuf;
 use clap::{arg, command, value_parser};
 
 fn main() {
-    let matches = command!()
+    command!()
         .arg(
             arg!(
                 -w --wildcard <FILE> "Wildcard name, the * in .github/workflows/wildcard-*"
@@ -12,10 +12,4 @@ fn main() {
             .value_parser(value_parser!(PathBuf)),
         )
         .get_matches();
-
-    if let Some(config_path) = matches.get_one::<PathBuf>("config") {
-        println!("Value for config: {}", config_path.display());
-    }
-
-    // Continued program logic goes here...
 }
