454e7efb31c244238b41cbf5997a0bc34779acc4
Antti Harju <antti@harju.io>
2025-11-24 22:49:41 +0100

    Remove test subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 
-use clap::{arg, command, value_parser, ArgAction, Command};
+use clap::{arg, command, value_parser};
 
 fn main() {
     let matches = command!()
@@ -11,27 +11,10 @@ fn main() {
             .required(true)
             .value_parser(value_parser!(PathBuf)),
         )
-        .subcommand(
-            Command::new("test")
-                .about("does testing things")
-                .arg(arg!(-l --list "lists test values").action(ArgAction::SetTrue)),
-        )
         .get_matches();
 
     if let Some(config_path) = matches.get_one::<PathBuf>("config") {
         println!("Value for config: {}", config_path.display());
-    }
-
-    // You can check for the existence of subcommands, and if found use their
-    // matches just as you would the top level cmd
-    if let Some(matches) = matches.subcommand_matches("test") {
-        // "$ myapp test" was run
-        if matches.get_flag("list") {
-            // "$ myapp test -l" was run
-            println!("Printing testing lists...");
-        } else {
-            println!("Not printing testing lists...");
-        }
     }
 
     // Continued program logic goes here...
