ca4b8acab6961df066119db386fcf694b2fcebf3
Antti Harju <antti@harju.io>
2025-11-24 22:57:27 +0100

    Add about

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use clap::{arg, command, value_parser};
 
 fn main() {
     command!()
+        .about(
+            "Compare an array of JSON changes to on.push.paths of a .github/workflows/wildcard-* file.",
+        )
         .arg(
             arg!(
                 -w --wildcard <FILE> "Wildcard name, the * in .github/workflows/wildcard-*"
