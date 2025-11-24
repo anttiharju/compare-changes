5f31d0fdae3cb767bd46a4f38771fcb73181f53f
Antti Harju <antti@harju.io>
2025-11-24 23:09:05 +0100

    Add a style

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
+use anstyle::{AnsiColor, Color, Style};
+use clap::{arg, builder::Styles, command, value_parser};
 use std::path::PathBuf;
 
-use clap::{arg, command, value_parser};
+fn get_styles() -> Styles {
+    Styles::styled()
+        .usage(
+            Style::new()
+                .bold()
+                .fg_color(Some(Color::Ansi(AnsiColor::Green))),
+        )
+        .header(
+            Style::new()
+                .bold()
+                .fg_color(Some(Color::Ansi(AnsiColor::Green))),
+        )
+        .literal(
+            Style::new()
+                .bold()
+                .fg_color(Some(Color::Ansi(AnsiColor::Cyan))),
+        )
+        .placeholder(Style::new().fg_color(Some(Color::Ansi(AnsiColor::Cyan))))
+}
 
 fn main() {
     command!()
-        .about(
-            "Compare an array of JSON changes to on.push.paths of a .github/workflows/wildcard-* file.",
-        )
+        .about("Compare an array of JSON changes to on.push.paths of a .github/workflows/wildcard-* file.")
+        .styles(get_styles())
         .arg(
             arg!(
                 -w --wildcard <FILE> "Wildcard name, the * in .github/workflows/wildcard-*"
