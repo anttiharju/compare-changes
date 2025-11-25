12f458a1d76ba373496e0baf2ee0ece4b3f65c80
Antti Harju <antti@harju.io>
2025-11-25 13:42:55 +0100

    Use serde-saphyr in an idiomatic way

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 use anstyle::{AnsiColor, Color, Style};
 use clap::{arg, builder::Styles, command, value_parser};
-use serde_yaml::Value;
+use serde::Deserialize;
 use std::fs;
 use std::path::PathBuf;
+
+#[derive(Debug, Deserialize)]
+struct Wildcard {
+    on: Option<On>,
+}
+
+#[derive(Debug, Deserialize)]
+struct On {
+    push: Option<Push>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Push {
+    paths: Option<Vec<String>>,
+}
 
 fn get_styles() -> Styles {
     Styles::styled()
@@ -49,18 +64,17 @@ fn main() {
         PathBuf::from(".github/workflows").join(format!("wildcard-{}", wildcard.display()));
     let wildcard_contents =
         fs::read_to_string(&wildcard_path).expect("Failed to read wildcard file");
-    let yaml: Value = serde_yaml::from_str(&wildcard_contents).expect("Failed to parse YAML");
+
+    let wildcard: Wildcard =
+        serde_saphyr::from_str(&wildcard_contents).expect("Failed to parse YAML");
+
     println!("{:#?}.on.push.paths:", wildcard_path);
-    if let Some(paths) = yaml
-        .get("on")
-        .and_then(|on| on.get("push"))
-        .and_then(|push| push.get("paths"))
-        .and_then(|paths| paths.as_sequence())
+    if let Some(paths) = wildcard
+        .on
+        .and_then(|on| on.push.and_then(|push| push.paths))
     {
         for path in paths {
-            if let Some(path_str) = path.as_str() {
-                println!("- {}", path_str);
-            }
+            println!("- {}", path);
         }
     } else {
         println!("No on.push.paths found.");
