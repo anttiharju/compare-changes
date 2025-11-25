b050f28d077f354cf8ee23a718f0fcd541c0c057
Antti Harju <antti@harju.io>
2025-11-25 14:08:10 +0100

    Fix cli test

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,16 +1,34 @@
 use assert_cmd::cargo::cargo_bin_cmd;
+use std::fs;
+use std::path::Path;
+use tempfile::tempdir;
 
 #[test]
 fn test_changes_output() {
-    let mut cmd = cargo_bin_cmd!("compare-changes");
-    cmd.arg("--wildcard")
+    let temp = tempdir().unwrap();
+    let workflows = temp.path().join(".github/workflows");
+    fs::create_dir_all(&workflows).unwrap();
+
+    let src = Path::new(env!("CARGO_MANIFEST_DIR")).join("tests/wildcard-test.yml");
+    let dst = workflows.join("wildcard-test.yml");
+    fs::copy(src, dst).unwrap();
+
+    let output = cargo_bin_cmd!("compare-changes")
+        .current_dir(&temp)
+        .arg("--wildcard")
         .arg("test.yml")
         .arg("--changes")
-        .arg(r#"["foo/bar", "baz"]"#);
+        .arg(r#"["foo/bar", "baz"]"#)
+        .output()
+        .unwrap();
 
-    let output = cmd.assert().success().get_output().stdout.clone();
-    let output_str = String::from_utf8_lossy(&output);
+    let stdout = String::from_utf8_lossy(&output.stdout);
 
-    assert!(output_str.contains("foo/bar"));
-    assert!(output_str.contains("baz"));
+    for expected in ["1", "2", "3", "foo/bar", "baz"] {
+        assert!(
+            stdout.contains(expected),
+            "Expected output to contain '{}'",
+            expected
+        );
+    }
 }
