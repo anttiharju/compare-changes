c5a1fe8cb2e36b6bb2bf81210e4baf5489aa655c
Antti Harju <antti@harju.io>
2025-11-25 10:28:15 +0100

    Fix zcc setup properly

--- .release/archive.sh
+++ .release/archive.sh
@@ -9,7 +9,7 @@ echo "$0 $tag $target"
 
 rm -rf "tmp/$target"
 repo="$(yq -p toml -oy '.package.name' "$repo_root/Cargo.toml")"
-cargo build --target "$target" --release
+CC=".release/.zcc/$target.sh" cargo build --target "$target" --release
 
 cd "target/$target/release"
 tar -czf "$repo_root/$repo-$target.tar.gz" "$repo"
