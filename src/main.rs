067582e70f718d5b1adae27d9905a94d79bf2968
Antti Harju <antti@harju.io>
2025-11-26 10:46:36 +0100

    Basic library integration

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,40 @@ mod cli;
 mod exitcode;
 mod parse;
 
+use compare_changes::path_matches_at_least_one_file;
+
 fn main() {
     let args = cli::parse_args();
 
-    match parse::get_paths(&args.wildcard) {
+    let paths = match parse::get_paths(&args.wildcard) {
         Ok(paths) => {
             println!("{}.on.push.paths:", args.wildcard.display());
             for path in &paths {
                 println!("- {}", path);
             }
+            paths
         }
         Err(err) => {
             eprintln!("{}", err);
             exitcode::paths_error();
         }
-    }
+    };
 
-    match parse::get_files(&args.changes_json) {
+    let files = match parse::get_files(&args.changes_json) {
         Ok(files) => {
             println!("files:");
             for file in &files {
                 println!("- {}", file);
             }
+            files
         }
         Err(err) => {
             eprintln!("{}", err);
             exitcode::files_error();
         }
-    }
+    };
+
+    let file_refs: Vec<&str> = files.iter().map(|s| s.as_str()).collect();
+    let changed = paths.iter().any(|path| path_matches_at_least_one_file(path, &file_refs));
+    println!("changed={}", changed);
 }
