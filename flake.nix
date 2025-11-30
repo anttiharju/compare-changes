148614c41093bfdb45e4307cd7b30dfcfe13c364
Antti Harju <antti@harju.io>
2025-11-30 23:25:00 +0200

    Stop compiling macOS images

--- flake.nix
+++ flake.nix
@@ -45,7 +45,6 @@
               "rustfmt"
               "rust-src"
             ])
-            fenix.packages.${system}.targets.aarch64-apple-darwin.stable.rust-std
             fenix.packages.${system}.targets.aarch64-unknown-linux-gnu.stable.rust-std
             fenix.packages.${system}.targets.x86_64-unknown-linux-gnu.stable.rust-std
           ];
@@ -148,7 +147,6 @@
             config = {
               User = "1001"; # https://github.com/actions/runner/issues/2033#issuecomment-1598547465
               Env = [
-                "CC_aarch64-apple-darwin=/zcc/aarch64-apple-darwin.sh"
                 "CC_aarch64-unknown-linux-gnu=/zcc/aarch64-unknown-linux-gnu.sh"
                 "CC_x86_64-unknown-linux-gnu=/zcc/x86_64-unknown-linux-gnu.sh"
                 "NIX_LD_LIBRARY_PATH=${
@@ -195,7 +193,6 @@
 
               # Install zig cc wrappers to /zcc
               mkdir -p /zcc
-              install -D -m755 ${zcc_scripts}/bin/aarch64-apple-darwin.sh /zcc/aarch64-apple-darwin.sh
               install -D -m755 ${zcc_scripts}/bin/aarch64-unknown-linux-gnu.sh /zcc/aarch64-unknown-linux-gnu.sh
               install -D -m755 ${zcc_scripts}/bin/x86_64-unknown-linux-gnu.sh /zcc/x86_64-unknown-linux-gnu.sh
             '';
