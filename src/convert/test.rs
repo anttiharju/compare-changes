076e28744ee34b522baf7bc4556c5d3fce6f2ae5
Antti Harju <antti@harju.io>
2025-11-30 15:00:43 +0200

    Fix another incorrect assumption

--- src/convert/test.rs
+++ src/convert/test.rs
@@ -50,10 +50,10 @@ fn test_double_star_wildcard() {
     // Edge case
     assert_path_match("**/a+", "a", true); // DoubleStar + Literal + QuestionMark, except the Literal would become empty as / gets stripped by DoubleStar
     assert_path_match("**/", "dir/", true); // Although git does not support committing (therefore pushing) empty directories
+    assert_path_match("**/", "file.txt", true);
     assert_path_match("**/", "dir/file.txt", true);
     assert_path_match("**/*", "dir/file.txt", true);
     assert_path_match("**/*", "dir/another/file.txt", true);
-    assert_path_match("**/", "file.txt", false);
 }
 
 #[test]
