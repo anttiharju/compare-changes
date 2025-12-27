6357d7777c07399c7374ab9479846c912ec8e39b
Antti Harju <antti@harju.io>
2025-12-27 13:24:23 +0200

    Fix render.sh

--- .release/render.sh
+++ .release/render.sh
@@ -16,7 +16,7 @@ fi
 [[ " $* " =~ " --no-cache " ]] && export NO_CACHE=1
 
 # Setup env
-[[ -z "$GITHUB_REPOSITORY" ]] && source actions_env_mock.sh
+[[ -z "${GITHUB_REPOSITORY:-}" ]] && source actions_env_mock.sh
 
 calculate_hash() {
   local file="$1"
