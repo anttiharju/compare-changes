6c1ae93400e17f6da299dac2dfdd562dff83d060
Antti Harju <antti@harju.io>
2025-12-27 20:24:43 +0200

    Fix indentation

--- .cargo/zcc/aarch64-apple-darwin.sh
+++ .cargo/zcc/aarch64-apple-darwin.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 # Filter out -liconv since it being included is "kind of an accident", see https://github.com/rust-lang/rust/issues/112501#issuecomment-1616996273
 ARGS=()
 for arg in "$@"; do
-    if [[ "$arg" != "-liconv" ]]; then
-        ARGS+=("$arg")
-    fi
+  if [[ "$arg" != "-liconv" ]]; then
+    ARGS+=("$arg")
+  fi
 done
 
 exec zig cc "${ARGS[@]}" -target aarch64-macos
