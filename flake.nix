69bcacf52b47b4b1af1cdf714b3c8a8aa8cb31f7
Antti Harju <antti@harju.io>
2026-04-30 17:00:36 +0300

    Fix flake desc

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Rust development environment";
+  description = "Rust development environment for compare-changes";
 
   nixConfig.extra-substituters = [
     "https://nix-community.cachix.org"
