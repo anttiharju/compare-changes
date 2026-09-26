47b9705888b72ac7fff8171a038bb6472313a0a3
Antti Harju <antti@harju.io>
2026-09-26 19:51:34 +0300

    Install check is not meant to fail

--- .release/.actions/scripts/install-check.sh
+++ .release/.actions/scripts/install-check.sh
@@ -8,16 +8,14 @@ cd "$directory"
 
 if [[ -f ./compare-changes ]]; then
   echo 'File check passed: compare-changes exists'
+  if [[ -x ./compare-changes ]]; then
+    echo 'Execute permission check passed: compare-changes is executable'
+  else
+    echo 'Execute permission check failed: compare-changes is not executable'
+    chmod +x compare-changes
+  fi
 else
   echo 'File check failed: no compare-changes found'
-  exit 1
-fi
-
-if [[ -x ./compare-changes ]]; then
-  echo 'Execute permission check passed: compare-changes is executable'
-else
-  echo 'Execute permission check failed: compare-changes is not executable'
-  chmod +x compare-changes
 fi
 
 if [[ -f ./compare-changes && -x ./compare-changes ]]; then
